cd57d5243a90053f7e7fb2d29f486f28dd97260f
thiagosir <thiagobento9@hotmail.com>
2023-11-27 15:51:09 -0300

    Tipagem de objetos

--- app.ts
+++ app.ts
@@ -3,3 +3,35 @@ function soma(a:number, b: number) {
 }
 
 soma(3, 1)
+
+
+// Tipagem de variaveis
+
+// Criando uma interface para 'Pessoa'
+interface Pessoa {
+  nome: string,
+  idade: number,
+  profissao?: string
+}
+
+const pessoa: Pessoa = {
+  nome: 'Thiago',
+  idade: 22
+}
+const outraPessoa: Pessoa = {
+  nome: 'Babi',
+  idade: 23
+}
+
+const arrayPessoa: Pessoa[] = [ // Array<Pessoa> é a mesma coisa de Pessoa[]
+  pessoa,
+  outraPessoa
+]
+
+const arrayNumber: number[] = [
+  1,2,3
+]
+
+const arrayString: Array<string> = [
+  'a', 'b', 'c'
+]
