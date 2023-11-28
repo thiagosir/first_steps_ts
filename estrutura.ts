b364d5a0f766c16214e024d80102b4230a1bf39f
thiagosir <thiagobento9@hotmail.com>
2023-11-28 16:38:03 -0300

    repeticoes

--- estrutura.ts
+++ estrutura.ts
@@ -1,7 +1,21 @@
-const num: number = 15
+// const num: number = 13
 
-if (num >= 15) {
-  console.log('numero maior que 15')
-} else {
-  console.log('numero menor que 15')
+// if (num > 15) {
+//   console.log('numero maior que 15')
+// } else {
+//   console.log('numero menor que 15')
+// }
+
+const typeUser = {
+  admin: 'seja bem  vindo admin',
+  student: 'vc é estudante',
+  viewer: 'vc pode visualizar'
 }
+
+const usuario = 'admin'
+
+function validateUser(user: string) {
+  return console.log(typeUser[user as keyof typeof typeUser])
+}
+
+validateUser(usuario);
